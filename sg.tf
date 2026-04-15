f97632288f20809b94edb88ac80d2bc0ca6c9607
ahmadbinmohsin <133915077+ahmadbinmohsin@users.noreply.github.com>
2026-04-15 15:53:24 +0500

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -7,7 +7,7 @@ resource "null_resource" "example" {
         from_port   = 22
         to_port     = 22
         protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
+        cidr_blocks = ["10.0.0.0/16"]
       }]
       egress = [{
         from_port   = 0
